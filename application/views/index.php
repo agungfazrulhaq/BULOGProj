9a6ab9cf6f4e7b44052f68b286c8d2ff78642d0d
Muhammad Fachrizal Ramdani <fachrizalmr@users.noreply.github.com>
2020-06-12 12:46:21 +0700

    asd

--- application/views/index.php
+++ application/views/index.php
@@ -31,7 +31,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
-        <div class="row mb-2">
+        <div class="row">
           <div class="col-sm-6">
             <h1>Mutasi Kas Opaset</h1>
           </div>
