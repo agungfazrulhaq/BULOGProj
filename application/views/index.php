d3e3bb3617904defc110c13e9b368863bc4fc34e
Muhammad Fachrizal Ramdani <fachrizalmr@users.noreply.github.com>
2020-06-15 09:06:41 +0700

    asdfasd

--- application/views/index.php
+++ application/views/index.php
@@ -12,7 +12,7 @@
    <!-- Tempusdominus Bootstrap 4 -->
    <link rel="stylesheet" href="<?php echo base_url('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')?>">
    <!-- Toastr -->
-  <link rel="stylesheet" href="<?php echo base_url('plugins/toastr/toastr.min.cs')?>">
+  <link rel="stylesheet" href="<?php echo base_url('plugins/toastr/toastr.min.css')?>">
    <!-- Select2 -->
    <link rel="stylesheet" href="<?php echo base_url('plugins/select2/css/select2.min.css')?>">
    <link rel="stylesheet" href="<?php echo base_url('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css')?>">
