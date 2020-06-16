2d1eff814303914bb8e216a9b8264a8362a9b15c
agungfazrulhaq <agungfazrulhaq95@gmail.com>
2020-06-16 07:53:50 +0700

    Update index.php

--- application/views/index.php
+++ application/views/index.php
@@ -346,7 +346,7 @@
               <div class="card-body p-0">
                 <div class="table-responsive">
                 <div class="card-body">
-                  <table class="table table-hover table-sm" style="text-align: center;" id="tbmaster">
+                  <table class="table table-hover table-sm"  style="text-align: center;" id="example1">
                     <thead class="" >
                       <tr>
                           <td><b>AKSI</b></td>
@@ -724,7 +724,7 @@
                     ?>
 
   $(function () {
-    $("#tbmaster").DataTable({
+    $("#example1").DataTable({
       "responsive": true,
       "autoWidth": true,
       "searching": true,
@@ -852,6 +852,79 @@ function formatRupiah(angka, prefix) {
   return prefix == undefined ? rupiah : rupiah ? " " + rupiah : "";
 }
 </script>
+<script>
+    $(document).ready(function(){
+        // Setup datatables
+        $.fn.dataTableExt.oApi.fnPagingInfo = function(oSettings)
+      {
+          return {
+              "iStart": oSettings._iDisplayStart,
+              "iEnd": oSettings.fnDisplayEnd(),
+              "iLength": oSettings._iDisplayLength,
+              "iTotal": oSettings.fnRecordsTotal(),
+              "iFilteredTotal": oSettings.fnRecordsDisplay(),
+              "iPage": Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength),
+              "iTotalPages": Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength)
+          };
+      };
+ 
+      var table = $("#mytable").dataTable({
+          initComplete: function() {
+              var api = this.api();
+              $('#mytable_filter input')
+                  .off('.DT')
+                  .on('input.DT', function() {
+                      api.search(this.value).draw();
+              });
+          },
+              oLanguage: {
+              sProcessing: "loading..."
+          },
+              processing: true,
+              serverSide: true,
+              ajax: {"url": "<?php echo base_url().'index.php/crud/get_produk_json'?>", "type": "POST"},
+                    columns: [
+                                                {"data": "barang_kode"},
+                                                {"data": "barang_nama"},
+                                                //render harga dengan format angka
+                        {"data": "barang_harga", render: $.fn.dataTable.render.number(',', '.', '')},
+                        {"data": "kategori_nama"},
+                        {"data": "view"}
+                  ],
+                order: [[1, 'asc']],
+          rowCallback: function(row, data, iDisplayIndex) {
+              var info = this.fnPagingInfo();
+              var page = info.iPage;
+              var length = info.iLength;
+              $('td:eq(0)', row).html();
+          }
+ 
+      });
+            // end setup datatables
+            // get Edit Records
+            $('#mytable').on('click','.edit_record',function(){
+            var kode=$(this).data('kode');
+                        var nama=$(this).data('nama');
+                        var harga=$(this).data('harga');
+                        var kategori=$(this).data('kategori');
+            $('#ModalUpdate').modal('show');
+            $('[name="kode_barang"]').val(kode);
+                        $('[name="nama_barang"]').val(nama);
+                        $('[name="harga"]').val(harga);
+                        $('[name="kategori"]').val(kategori);
+      });
+            // End Edit Records
+            // get Hapus Records
+            $('#mytable').on('click','.hapus_record',function(){
+            var kode=$(this).data('kode');
+            $('#ModalHapus').modal('show');
+            $('[name="kode_barang"]').val(kode);
+      });
+            // End Hapus Records
+ 
+    });
+</script>
 <script src="<?php echo base_url();?>dist/js/demo.js"></script>
 </body>
+
 </html>
