2375f3be657f55601cf7756581e645c6d0a3a10f
Muhammad Fachrizal Ramdani <fachrizalmr@users.noreply.github.com>
2020-06-19 09:36:39 +0700

    Modal Fixed
    asdas

--- application/views/index.php
+++ application/views/index.php
@@ -37,13 +37,14 @@
   .page-link {
     color: black !important;
   }
+  
 </style>
 
 <body class="layout-top-nav" style="height: auto;">
 
   <!-- ALL Modal Control -->
-  <div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-              <div class="modal-dialog" role="document">
+      <div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                   <form action="<?php echo site_url('Home/add') ?>" method="post" enctype="multipart/form-data">
                     <div class="modal-header text-center">
@@ -121,7 +122,7 @@
             </div>
 
   <div class="modal fade" id="modalView" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-xl" role="document">
+    <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
       <div class="modal-content">
         <div class="card card-info card-outline">
           <div class="card-header font-weight-bold">
@@ -192,7 +193,7 @@
     <div class="col-md-2">
       <form action="<?php echo site_url('Home/update/') ?>" method="post" enctype="multipart/form-data">
         <div class="modal fade" id="modalUpdate" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-          <div class="modal-dialog" role="document">
+          <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
               <div class="modal-header text-center">
                 <h4 class="modal-title font-weight-bold">Ubah Data [Transaksi]</h4>
@@ -263,7 +264,7 @@
     <div class="col-md-2">
       <form action="" method="post" enctype="multipart/form-data">
         <div class="modal fade" id="modalCetak" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-          <div class="modal-dialog" role="document">
+          <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
               <div class="modal-header text-center">
                 <h4 class="modal-title font-weight-bold">Cetak Laporan Keuangan</h4>
@@ -361,7 +362,7 @@
   </div>
 
   <div class="modal fade" id="modaldel">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
           <h3 class="modal-title"><b>Konfimasi Hapus Data Transaksi</b></h3>
@@ -495,7 +496,7 @@
           <div class="col-md-2">
               <div class="mb-2">
             <button type="button" class="btn btn-info" data-toggle="modal" data-target="#modalLoginForm"><span data-toggle="tooltip" title="Tambahkan Data" data-placement="top"> Masukkan Data Transaksi</span></button>
-            <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modalkategori"><i class="fas fa-plus" data-toggle="tooltip" title="Tambah Kategori Uraian" data-placement="top"></i></button>
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#modalkategori"><i class="fas fa-plus" data-toggle="tooltip" title="Tambah Kategori Uraian" data-placement="top"></i></button>
               </div>
             <div class="card">
               <div class="card-header">
@@ -520,7 +521,8 @@
                     }
                     ?>
                     <li class="nav-item">
-                      <a href="<?php echo site_url($link_showaset); ?>" id="namaaset<?php echo $row_a->id_aset; ?>" style="border-radius:0px;" class="nav-link"> <?php echo $row_a->nama_aset; ?></a>
+                      <a href="<?php echo site_url($link_showaset); ?>" id="namaaset<?php echo $row_a->id_aset; ?>" style="border-radius:0px;" class="nav-link"> <?php echo $row_a->nama_aset; ?>
+                       <span class="float-right btn btn-xs "><i class="fas fa-times text-red"></i></span></a>
                     </li>
                   <?php } ?>
                   <li class="nav-item">
