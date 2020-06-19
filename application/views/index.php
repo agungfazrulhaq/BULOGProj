74b487d3ab22f5e0aa783cafcc23bb733d6703ad
Muhammad Fachrizal Ramdani <fachrizalmr@users.noreply.github.com>
2020-06-19 10:33:46 +0700

    1234

--- application/views/index.php
+++ application/views/index.php
@@ -543,7 +543,7 @@
               <div class="card-header">
                 <h3 class="card-title"><b>Rekap Tahun</b></h3>
                 <div class="card-tools">
-                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
                   </button>
                 </div>
               </div>
