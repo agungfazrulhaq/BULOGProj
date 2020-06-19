3019b0144c3f76a18e66478ea985ae9fdd27f16d
Agung Rizky <agungrizky.8g@gmail.com>
2020-06-19 22:07:59 +0800

    hgcfhgfc

--- database/db_keuangan_opaset_sulselbar.sql
+++ database/db_keuangan_opaset_sulselbar.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 19, 2020 at 09:42 AM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.3.1
+-- Generation Time: Jun 19, 2020 at 04:07 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.3.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -34,13 +33,6 @@ CREATE TABLE `files` (
   `id_transaksi` int(11) DEFAULT NULL,
   `ukuran_file` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `files`
---
-
-INSERT INTO `files` (`id_files`, `nama_file`, `id_transaksi`, `ukuran_file`) VALUES
-(3, '1301171750_Laporan.pdf', 15, 423525);
 
 -- --------------------------------------------------------
 
@@ -97,7 +89,7 @@ CREATE TABLE `tb_transaksi` (
   `saldo` double NOT NULL,
   `transaksi_id_aset` int(11) DEFAULT NULL,
   `transaksi_id_kategori` int(11) DEFAULT NULL,
-  `waktuupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `waktuupdate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -105,9 +97,19 @@ CREATE TABLE `tb_transaksi` (
 --
 
 INSERT INTO `tb_transaksi` (`tahun`, `tanggal`, `id_transaksi`, `uraian`, `ref`, `saldo`, `transaksi_id_aset`, `transaksi_id_kategori`, `waktuupdate`) VALUES
-(2020, '2020-03-30', 15, 'asdasdasd', 'KREDIT', 55444, 1, 2, '2020-06-19 06:57:09'),
-(2020, '2020-06-15', 18, 'AAAAA <b> BB </b>', 'DEBIT', 221311, 2, 1, '2020-06-19 06:57:09'),
-(2020, '2020-06-09', 19, 'asasasdasdw', 'DEBIT', 22222, 1, 3, '2020-06-19 07:24:00');
+(2019, '2019-11-10', 20, 'Sewa Gedung &amp; Charge Foto/Shooting (Hj. Rahmatia)<br>', 'DEBIT', 6450000, 1, 2, '2020-06-19 08:29:53'),
+(2019, '2019-11-14', 21, '<p>Sewa Gedung &amp; Charge Foto/Shooting (FATIMAH)<br></p>', 'DEBIT', 7950000, 1, 2, '2020-06-19 08:32:45'),
+(2019, '2019-11-14', 22, '<p>Biaya pembayaran pemain elekton bulan Oktober 2019<br></p>', 'KREDIT', 4480000, 1, 3, '2020-06-19 08:36:18'),
+(2019, '2019-11-01', 23, '<p>Saldo Awal<br></p>', 'DEBIT', 14432643, 1, 3, '2020-06-19 08:39:04'),
+(2019, '2019-11-14', 24, '<p>Terima PPH 21<br></p>', 'DEBIT', 268800, 1, 3, '2020-06-19 08:40:39'),
+(2019, '2019-11-14', 25, '<p>Biaya Belanja Keperluan Gedung Baruga Lappo Ase Bulan November 2019<br></p>', 'KREDIT', 2000000, 1, 3, '2020-06-19 08:42:11'),
+(2019, '2019-11-14', 26, '<p>Biaya Pembelian Air Mineral Sebanyak 225 Dos Bulan Oktober 2019<br></p>', 'KREDIT', 3375000, 1, 3, '2020-06-19 08:44:08'),
+(2019, '2019-11-14', 27, '<p>Biaya Laundry Baruga Lappo Ase<br></p>', 'KREDIT', 525000, 1, 3, '2020-06-19 08:48:24'),
+(2019, '2019-11-14', 28, '<p>Biaya upah petugas kebersihan baruga lappo ase&nbsp; November a/n Adim Yusuf BBA<br></p>', 'KREDIT', 1040000, 1, 3, '2020-06-19 08:53:27'),
+(2019, '2019-11-14', 29, '<p>Terima PPH 21<br></p>', 'DEBIT', 72300, 1, 3, '2020-06-19 08:54:40'),
+(2019, '2019-12-14', 30, '<p>MUSSUARDI<br></p>', 'DEBIT', 1000000, 1, 3, '2020-06-19 08:56:07'),
+(2019, '2019-12-01', 31, '<p>INDRIANY<br></p>', 'DEBIT', 1000000, 1, 3, '2020-06-19 08:59:18'),
+(2019, '2019-12-01', 32, '<p>Terima dari fotografer mahkota Oktober 2019<br></p>', 'DEBIT', 2100000, 1, 3, '2020-06-19 09:01:20');
 
 --
 -- Indexes for dumped tables
@@ -166,7 +168,7 @@ ALTER TABLE `tb_kategori`
 -- AUTO_INCREMENT for table `tb_transaksi`
 --
 ALTER TABLE `tb_transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- Constraints for dumped tables
