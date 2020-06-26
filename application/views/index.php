446b3f87c21689e4f6bb3f5c1f399a382852d677
Muhammad Fachrizal Ramdani <fachrizalmr@users.noreply.github.com>
2020-06-26 09:25:28 +0700

    123

--- application/views/index.php
+++ application/views/index.php
@@ -602,7 +602,7 @@
         <div class="row">
           <div class="col-2" style="font-size:1vw;">
           <img class="img-fluid mb-2 shadow-sm bg-white p-2 rounded" width="100%" src="<?php echo base_url("dist/img/logo dash.png"); ?>" alt="Logo Bulog">
-            <button type="button" class="btn shadow-sm btn-block btn-info mb-2 font-weight-light" data-toggle="modal" data-target="#modalLoginForm"><span data-toggle="tooltip" title="Tambahkan Data" data-placement="top"> Masukkan Data Transaksi</span></button>
+            <button type="button" class="btn shadow-sm btn-block btn-info mb-2 font-weight-regular" data-toggle="modal" data-target="#modalLoginForm"><span data-toggle="tooltip" title="Tambahkan Data" data-placement="top"> Masukkan Data Transaksi</span></button>
             <div class="card">
               <div class="card-header">
                 <h3 class="card-title"><b>
