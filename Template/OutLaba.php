e8711207e9af321912639a2ee14d04f77820c9ec
Muhammad Fachrizal Ramdani <fachrizalmr@users.noreply.github.com>
2020-06-29 09:27:21 +0700

    outphp

---
+++ Template/OutLaba.php
@@ -0,0 +1,582 @@
+<!DOCTYPE html>
+<head>
+
+<style type="text/css">
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size:12px;
+}
+
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+
+th {
+  text-align: center;
+  height: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+
+.ram {
+  text-align:left;
+  padding-left:15px;
+}
+
+thead {
+  background-color:grey;
+}
+
+.tj{
+  background-color: #DCDCDC;
+}
+
+</style>
+</head>
+<body>
+<p><b> UB. OPASET DIVRE SULSELBAR <br>
+<br>
+LAPORAN&emsp;:&emsp;LABA (RUGI)<br>
+PERIODE&emsp; :&emsp;01 JANUARI - 31 OKTOBER 2019</b></p>
+  <table>
+    <thead>
+      <tr>
+        <th  rowspan="2">NO</th>
+        <th style="width:270px;"  rowspan="2">URAIAN</th>
+        <th>BARUGA</th>
+        <th>WISMA</th>
+        <th>GOR</th>
+        <th>MALINO</th>
+        <th>RUKO & LAHAN</th>
+        <th>SUBDIVRE PARE</th>
+        <th>SUBDIVRE PLP</th>
+        <th>SUBDIVRE MKSR</th>
+        <th>SUBDIVRE SULSEL</th>
+        <th>TOTAL JUMLAH</th>
+      </tr>
+      <tr>
+        <th >(Rp)</th>
+        <th >(Rp)</th>
+        <th >(Rp)</th>
+        <th >(Rp)</th>
+        <th >(Rp)</th>
+        <th >(Rp)</th>
+        <th >(Rp)</th>
+        <th >(Rp)</th>
+        <th >(Rp)</th>
+        <th >(Rp)</th>
+      </tr>
+      <tr>
+        <th >1</th>
+        <th >2</th>
+        <th >3</th>
+        <th >4</th>
+        <th >5</th>
+        <th >6</th>
+        <th >7</th>
+        <th >8</th>
+        <th >9</th>
+        <th >10</th>
+        <th >11</th>
+        <th >12</th>
+      </tr>
+    </thead>
+    <tbody>
+
+      <tr>
+        <td >&nbsp;</td>
+        <td >&nbsp;</td>
+        <td >&nbsp;</td>
+        <td >&nbsp;</td>
+        <td >&nbsp;</td>
+        <td >&nbsp;</td>
+        <td >&nbsp;</td>
+        <td >&nbsp;</td>
+        <td >&nbsp;</td>
+        <td >&nbsp;</td>
+        <td >&nbsp;</td>
+        <td class="tj">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="ram"><b>1</b></td>
+        <td ><b>PENDAPATAN</b></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram">1.1</td>
+        <td >SEWA ASSETS</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram">1.2</td>
+        <td >SEWA ASSETS dari PYD/Hotel</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td ></td>
+        <td ><b>Jumlah</b></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td ></td>
+        <td >&nbsp;</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td ></td>
+        <td ><b>LABA RUGI KOTOR</b></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td ></td>
+        <td >&nbsp;</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram"><b>2.2</b></td>
+        <td ><b>BIAYA UMUM</b></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram">2.2.1</td>
+        <td >Biaya Pegawai</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram">2.2.2</td>
+        <td >Biaya Kantor, ATK</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram">2.2.3</td>
+        <td >By Perjalanan Dinas</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram">2.2.4</td>
+        <td >Biaya perbaikan/pemeliharaan</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram">2.2.5</td>
+        <td >Biaya PPN</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram">2.2.7</td>
+        <td >Biaya Pajak PPh Pasal 4(2)</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram">2.2.8</td>
+        <td >Biaya Pajak PPh Pasal 21</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram">2.2.9</td>
+        <td >Biaya Pajak Restoran & Hotel</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram">2.2.10</td>
+        <td >PBB</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram">2.2.11</td>
+        <td >Biaya Lain-Lain</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td ></td>
+        <td ><b>Jumlah Biaya Umum</b></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td ></td>
+        <td >&nbsp;</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td ></td>
+        <td ><b>LABA RUGI USAHA</b></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td ></td>
+        <td >&nbsp;</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram"><b>1.2</b></td>
+        <td ><b>PENDAPATAN  DILUAR USAHA</b></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram">1.2.1</td>
+        <td >Pendapatan Bunga Bank</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram">1.2.2</td>
+        <td >Pendapatan Lain-lain</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td ></td>
+        <td >&nbsp;</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram"><b>2.3</b></td>
+        <td ><b>BIAYA BUNGA & PENYUSUTAN</b></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram">2.3.1</td>
+        <td >Biaya Bank</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram">2.3.2</td>
+        <td >Biaya ADM</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram">2.3.3</td>
+        <td >Biaya Penyusutan</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td ></td>
+        <td ><b>Jumlah Biaya Bunga dan Peenyusutan</b></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td ></td>
+        <td >&nbsp;</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td class="ram"><b>3</b></td>
+        <td ><b>LABA</b></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"></td>
+      </tr>
+      <tr>
+        <td ></td>
+        <td >&nbsp;</td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td class="tj"1></td>
+      </tr>
+
+    </tbody>
+  </table>
+</body>
+</html>
